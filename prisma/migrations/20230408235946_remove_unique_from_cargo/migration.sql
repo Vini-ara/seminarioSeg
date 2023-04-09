fdd3f3db7bcfa1c8d31bef40831bc6b99e36541c
Edson Sales <edsondesouzasales@gmail.com>
2023-04-08 21:01:44 -0300

    fix:corrige seed do prisma

---
+++ prisma/migrations/20230408235946_remove_unique_from_cargo/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX "Cargo_nome_key";
--- prisma/seed.js
+++ prisma/seed.js
@@ -3,9 +3,9 @@ const prisma = new PrismaClient();
 
 async function main () {
     // Criação dos Cargos
-    for (let nome of ['assessor', 'coordenador'])
+    for (let nome of ['assessor', 'coordenador', 'lider'])
         for (let nucleo of ['NDP', 'NIP', 'NOE', 'NAV', 'NUT'])
-            prisma.cargo.create({
+            await prisma.cargo.create({
                 data: {
                     nome,
                     nucleo,
