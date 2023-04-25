f9a776aa3fa848500a2e397b37d5632926b57e88
Vinicius Araujo <vinicius.ar02@gmail.com>
2023-04-24 21:50:16 -0300

    feat: adiciona icone de fundo da tela de cadastro e input de foto de perfil

--- public/cadastro/script.js
+++ public/cadastro/script.js
@@ -4,3 +4,21 @@ header({
   name: "Vinizada",
   img: "https://images.unsplash.com/photo-1587691592099-24045742c181?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
 }).render()
+
+var inputPreview = () => {
+    let preview = document.getElementById("profilePicInput")
+    let input = document.getElementById("imageInput")
+    const reader = new FileReader()
+
+    input.addEventListener("input", () => {
+        let file = input.files[0]
+
+        reader.readAsDataURL(file);
+
+        reader.addEventListener("load",()=> {
+            preview.src = reader.result;
+        }, false)
+    })
+}
+
+inputPreview()
